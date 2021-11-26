28f7ba7a35bac32fcffa475d5ca652ef32142582
Ariadne Bai <bairuoqi@yeah.net>
2021-11-25 23:01:56 -0500

    notice how variable types are passed into closures

--- rusty-links/src/second.rs
+++ rusty-links/src/second.rs
@@ -43,7 +43,13 @@ impl<T> List<T> {
     // impl<T> Option<T> {
     //     fn as_ref(&self) -> Option<&T>;
     // }
-    pub fn peek(&mut self) -> Option<&mut T> {
+    pub fn peek(& self) -> Option<& T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| {
             &mut node.elem
         })
@@ -59,4 +65,21 @@ impl<T> Drop for List<T> {
             cur_link = mem::replace(&mut box_node.next, None);
         }
     }
+}
+
+#[test]
+fn peek() {
+    let mut list = List::<i32>::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+    list.push(1); list.push(2); list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
+
+    list.peek_mut().map(|value| {
+        *value = 42;
+    });
+    assert_eq!(list.peek(), Some(&42));
+    assert_eq!(list.pop(), Some(42));
 }
